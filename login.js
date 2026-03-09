5dbed46f82b36b929556a601de7494d7a7f59b3a
3llaine <ezapata@xqcengineering.com>
2026-03-09 11:59:04 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,8 @@
+const gameText = document.getElementByld('game-text2');
+const userInput = document.getElementByld('user-input2');
+const submitBtn = document.getElementByld('submit-btn2');
+
+function print(text) {
+   gameText.innerText += text + "\n";
+   gameText.scrollTop = gameText.scrollHeight;
+}
